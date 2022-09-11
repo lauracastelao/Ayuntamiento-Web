fe189251d0e0c66d08bbf3594e4c7a8c6cd18cc1
Laura <lauragarciacastelao@hotmail.com>
2022-09-11 15:15:41 +0200

    First commit

---
+++ src/components/Access.vue
@@ -0,0 +1,19 @@
+<script setup>
+    defineProps({
+      msg: {
+        type: String,
+        required: true,
+      },
+    });
+    </script>
+
+
+<template>
+   
+  
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Access from '../components/Access.vue';
+import Footer from '../components/Copyright.vue';
+
+</script>
+
+<template>
+  <main>
+  <Access/>
+  <Footer/>
+  </main>
+</template>
